6e77c0f7007ef656cda7da9f3c131daf01315e01
Felix <felixlopzd@gmail.com>
2025-07-28 18:19:17 -0400

    adding analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "Felix Zepol | Alt Rock Artist",
@@ -13,6 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Analytics />
       <body>{children}</body>
     </html>
   );
