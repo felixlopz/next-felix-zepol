42a8a24e2a6c0c8a536f4909721e61fabfe7b72c
Felix Lopez <felix@Felixs-MacBook-Pro.local>
2025-07-28 18:13:27 -0400

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,103 +6,131 @@ import { Button } from "./button";
 import { SiApplemusic } from "react-icons/si";
 import { FaSpotify, FaTiktok, FaYoutube, FaInstagram } from "react-icons/fa6";
 
-import SVGAmazonMusic from "../../public/amazon.svg";
-import SVGDeezerMusic from "../../public/deezer.svg";
-
 export default function Home() {
   return (
-    <main className="relative flex flex-col items-center">
-      <div className="relative">
-        <div className="absolute left-0 top-0 w-full h-full bg-gradient-to-t from-[#130F0D]"></div>
-        <Image
-          src={"/banner.jpeg"}
-          alt="Felix Zepol Profile Pic"
-          width={2244}
-          height={2244}
-          className="sm:max-h-[400px] object-contain"
-        />
-      </div>
+    <main className="relative flex flex-col items-center py-5 ">
+      <Image
+        src={"/profile.jpeg"}
+        alt="Felix Zepol Profile Pic"
+        width={200}
+        height={200}
+        className="rounded-full mb-8 shadow-lg shadow-black/20"
+      />
 
-      <div className="max-w-[144px] text-center -mt-[84px] z-10">
+      <div className="max-w-[144px] text-center z-10">
         <h1 className={cn("text-white text-6xl", righteous.className)}>
           Felix Zepol
         </h1>
       </div>
 
-      <div className="flex flex-col mt-12 gap-y-4">
+      <div className="flex items-center gap-x-0 mt-4">
         <a
           href="https://open.spotify.com/artist/1sWmDanlClscGCuHLlWvOc"
+          className="p-4"
           target="_blank"
+          title="Spotify"
         >
-          <Button variant="spotify">
-            <FaSpotify className="mr-4" size={28} />
-            Spotify
-          </Button>
+          <FaSpotify className="text-white" size={36} />
         </a>
-
         <a
-          href="https://music.apple.com/us/artist/felix-zepol/1774570020"
+          href="https://music.apple.com/ve/artist/felix-zepol/1774570020"
+          className="p-4"
           target="_blank"
+          title="Apple Music"
         >
-          <Button variant="apple">
-            <SiApplemusic className="mr-4" size={28} />
-            Apple
-          </Button>
+          <SiApplemusic className="text-white" size={36} />
         </a>
-
-        <a
-          href="https://music.amazon.com/artists/B0DK811CHZ/felix-zepol"
-          target="_blank"
-        >
-          <Button variant="amazon">
-            <Image
-              src={SVGAmazonMusic}
-              alt="Amazon Music Logo, Felix Zepol"
-              width={28}
-              className="mr-4"
-            />
-            Amazon
-          </Button>
-        </a>
-        <a href="https://www.deezer.com/mx/artist/286432631" target="_blank">
-          <Button variant="deezer">
-            <Image
-              src={SVGDeezerMusic}
-              alt="Deezer Music Logo, Felix Zepol"
-              width={28}
-              className="mr-4"
-            />
-            Deezer
-          </Button>
-        </a>
-        {/* <a href="">
-          <Button variant="tidal">
-            <SiTidal className="mr-4" size={28} />
-            Tidal
-          </Button>
-        </a> */}
-      </div>
-      <div className="flex items-center gap-x-2 mt-8 mb-89">
         <a
           href="https://www.youtube.com/@felixzepol"
           className="p-4"
           target="_blank"
+          title="YouTube"
         >
-          <FaYoutube className="text-white" size={24} />
+          <FaYoutube className="text-white" size={36} />
         </a>
         <a
           href="https://www.instagram.com/felix.zepol"
           className="p-4"
           target="_blank"
+          title="Instagram"
         >
-          <FaInstagram className="text-white" size={24} />
+          <FaInstagram className="text-white" size={36} />
         </a>
         <a
           href="https://www.tiktok.com/@felix.zepol"
           className="p-4"
           target="_blank"
+          title="TikTok"
         >
-          <FaTiktok className="text-white" size={24} />
+          <FaTiktok className="text-white" size={36} />
+        </a>
+      </div>
+      <h3 className={cn("text-white text-2xl mb-5", righteous.className)}>
+        Discografía
+      </h3>
+      <div className="">
+        <a
+          href="#"
+          target="_blank"
+          title="Poesía Moderna - 15/08"
+          className="relative flex items-center gap-x-2 mb-4  rounded-sm  w-[325px] bg-stone-900 px-4 py-2"
+        >
+          <div className="absolute text-white bg-red-600 p-2 rounded-sm rotate-12 -top-5 -right-5 animate-pulse">
+            <span className="text-xs">Nuevo</span>
+          </div>
+          <Image
+            src={"/pm.png"}
+            alt="Ep Dia Cero"
+            width={50}
+            height={50}
+            className="rounded-sm "
+          />
+          <p className="text-white">Poesía Moderna OUT 15/08</p>
+        </a>
+        <a
+          href="https://open.spotify.com/album/67e0F9GxAvxqCEswJMgE3H"
+          target="_blank"
+          title="EP: Día Cero"
+          className="flex items-center gap-x-2 mb-4  rounded-sm  w-[325px] bg-stone-900 px-4 py-2"
+        >
+          <Image
+            src={"/dia-cero.png"}
+            alt="Ep Dia Cero"
+            width={50}
+            height={50}
+            className="rounded-sm "
+          />
+          <p className="text-white">EP: Día Cero</p>
+        </a>
+        <a
+          href="https://open.spotify.com/album/4OKkA4NtQnD712iONYMDK9?si=xbVf0c0sS6q-SCjOEM4kxA"
+          target="_blank"
+          title="El Principio Del Final"
+          className="flex items-center gap-x-2 mb-4  rounded-sm  w-[325px] bg-stone-900 px-4 py-2"
+        >
+          <Image
+            src={"/epdf.png"}
+            alt="El Principio Del Final"
+            width={50}
+            height={50}
+            className="rounded-sm "
+          />
+          <p className="text-white">El Principio Del Final</p>
+        </a>
+        <a
+          href="https://open.spotify.com/album/4lGxKQnW9QebHNng9EZxmz"
+          target="_blank"
+          title="Indomables"
+          className="flex items-center gap-x-2 mb-4  rounded-sm  w-[325px] bg-stone-900 px-4 py-2"
+        >
+          <Image
+            src={"/indomables.png"}
+            alt="Indomables"
+            width={50}
+            height={50}
+            className="rounded-sm "
+          />
+          <p className="text-white">Indomables</p>
         </a>
       </div>
     </main>
